16fcb720a47f7caaa1b0257a37818be8326f5ac5
Vlad Krasnov <vlad@cloudflare.com>
2016-01-09 06:53:14 -0800

    Add TLS Dynamic Record Resizing
    What we do now:
    We use a static record size of 4K. This gives a good balance of latency and
    throughput.
    
    Optimize latency:
    By initialy sending small (1 TCP segment) sized records, we are able to avoid
    HoL blocking of the first byte. This means TTFB is sometime lower by a whole
    RTT.
    
    Optimizing throughput:
    By sending increasingly larger records later in the connection, when HoL is not
    a problem, we reduce the overhead of TLS record (29 bytes per record with
    GCM/CHACHA-POLY).
    
    Logic:
    Start each connection with small records (1369 byte default, change with
    ssl_dyn_rec_size_lo). After a given number of records (40, change with
    ssl_dyn_rec_threshold) start sending larger records (4229, ssl_dyn_rec_size_hi).
    Eventually after the same number of records, start sending the largest records
    (ssl_buffer_size).
    In case the connection idles for a given amount of time (1s,
    ssl_dyn_rec_timeout), the process repeats itself (i.e. begin sending small
    records again).

--- src/http/modules/ngx_http_ssl_module.h
+++ src/http/modules/ngx_http_ssl_module.h
@@ -64,6 +64,12 @@ typedef struct {
     ngx_flag_t                      stapling_verify;
     ngx_str_t                       stapling_file;
     ngx_str_t                       stapling_responder;
+
+    ngx_flag_t                      dyn_rec_enable;
+    ngx_msec_t                      dyn_rec_timeout;
+    size_t                          dyn_rec_size_lo;
+    size_t                          dyn_rec_size_hi;
+    ngx_uint_t                      dyn_rec_threshold;
 } ngx_http_ssl_srv_conf_t;
 
 
