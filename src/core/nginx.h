75546f93358b652c22ca59bc3723be723a2c619d
Maxim Dounin <mdounin@mdounin.ru>
2024-02-19 03:12:06 +0300

    Free nginx: changed server name.

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -11,7 +11,8 @@
 
 #define nginx_version      1027004
 #define NGINX_VERSION      "1.27.4"
-#define NGINX_VER          "nginx/" NGINX_VERSION
+#define NGINX_NAME         "nginx"
+#define NGINX_VER          NGINX_NAME "/" NGINX_VERSION
 
 #ifdef NGX_BUILD
 #define NGINX_VER_BUILD    NGINX_VER " (" NGX_BUILD ")"
